661a00f954de4772ee5875ae3eed5b150d646bcb
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2019-06-20 21:05:43 +0530

    Styles and questions added.

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,18 @@ const initialState = {
     question_and_answers: [
             {
                 id: 1,
-                question: "What is the HTML tag under which one can write the JavaScript code?",
-                options: [" <javascript>", "<scripted>", "<script>", "<js>"],
-                correct_answer: 2
+                question: "The web development environment (JavaScript) offers which standard construct for data validation of the input entered by the user.",
+                options: [" Controlled loop constructs", "Case checking constructs", "Validation constructs", "All of the mentioned"],
+                correct_answer: 3
             },
             {
                 id: 2,
+                question: "Which attribute is used to specify that the script is executed when the page has finished parsing?",
+                options: ["parse", "async", "defer", "type"],
+                correct_answer: 2
+            },
+            {
+                id: 3,
                 question: "Which of the following is not a reserved word in JavaScript ?",
                 options: ["interface", "program", "throws", "short"],
                 correct_answer: 1
